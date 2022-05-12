145da439a3174545b56666cfee2b62d0d397d75b
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-05-12 20:55:21 +0000

    init

--- tauCircumference.cpp
+++ tauCircumference.cpp
@@ -6,22 +6,36 @@
 //     with dimensions inputted from user
 
 #include <iostream>
+#include <string>
 
 int main() {
-    // I calculate circumference of a circle
-    const double TAU = 6.28;
-    int radius;
-    double circumference;
+    // I calculate the factorial
+    std::string numberAsString;
+    int numberAsInt;
+    int i = 1;
+    int sumNumber = 1;
 
     // input
-    std::cout << "Enter radius of the circle in mm: ";
-    std::cin >> radius;
-
-    // process
-    circumference = TAU*radius;
-
-    // output
+    std::cout << "Enter a positive integer: ";
+    std::cin >> numberAsString;
     std::cout << "" << std::endl;
-    std::cout << "Circumference is " << circumference << " mm" << std::endl;
+
+    // process & output
+    try {
+        numberAsInt = std::stoi(numberAsString);
+        if (numberAsInt == 0) {
+            std::cout << "0! = 1." << std::endl;
+        } else if (numberAsInt < 0) {
+            std::cout << "undefined." << std::endl;
+        } else {
+            do {
+                sumNumber = sumNumber * i;
+                i += 1;
+            } while (i <= numberAsInt);
+            std::cout << "The sum is " << sumNumber << "." << std::endl;
+        }
+    } catch (std::invalid_argument) {
+        std::cout << "Invalid input" << std::endl;
+    }
     std::cout << "\nDone." << std::endl;
 }
